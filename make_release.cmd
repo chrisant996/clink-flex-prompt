7d493e90a740aa1e198f5737930cee4d1aef2ba3
Chris Antos <sparrowhawk996@gmail.com>
2021-09-18 16:13:06 -0700

    Add `make_release` script.
    Ignore *.zip files.

---
+++ make_release.cmd
@@ -0,0 +1,13 @@
+@echo off
+if "%1" == "" goto :usage
+
+set SEVENZ="c:\Program Files\7-Zip\7z.exe"
+
+%SEVENZ% a clink-flex-prompt-%1.zip flexprompt.lua flexprompt_modules.lua flexprompt_wizard.lua
+goto :eof
+
+:usage
+echo Usage:  make_release ^<version_number^>
+echo.
+echo   Creates a clink-flex-prompt-^<version_number^>.zip release file.
+goto :eof
