chrisant996/clink-flex-prompt

commit 22a12c1781268bb356590326d0d29e633aa1649c

Make `"connector"` separator work for rainbow and lean.

flexprompt.lua
@@ -655,11 +655,7 @@ local function init_segmenter(side, frame_color)
 
         if segmenter.style == "classic" then
             if type(separators) ~= "table" then
-                if separators == "connector" then
-                    separators = sgr(flexprompt.colors.default.bg .. ";" .. get_best_fg(segmenter.frame_color[fc_frame])) .. get_connector()
-                else
-                    separators = available_separators[separators] or separators
-                end
+                separators = available_separators[separators] or separators
             end
         elseif segmenter.style == "rainbow" then
             if type(separators) ~= "table" then
@@ -667,7 +663,6 @@ local function init_segmenter(side, frame_color)
                 if altseparators then
                     segmenter.altseparator = altseparators[side + 1]
                 end
-                separators = available_caps[separators] or available_caps["flat"]
             end
 
             -- Convert end cap index to separator index.
@@ -681,7 +676,11 @@ local function init_segmenter(side, frame_color)
         segmenter.separator = separators
     end
 
-    segmenter.separator = resolve_color_codes(segmenter.separator, "")
+    if segmenter.separator == "connector" then
+        segmenter.separator = sgr(flexprompt.colors.default.bg .. ";" .. get_best_fg(segmenter.frame_color[fc_frame])) .. get_connector()
+    else
+        segmenter.separator = resolve_color_codes(segmenter.separator, "")
+    end
 end
 
 local function color_segment_transition(color, symbol, close)

commit 442543f1059391a40fecadda305f170ed5b3e94f

Fix #11; rainbow style shows separator name by mistake.
Commit 22a12c17 accidentally broke that by removing too much of a line.

flexprompt.lua
@@ -593,7 +593,6 @@ end
 local function init_segmenter(side, frame_color)
     local charset = get_charset()
     local open_caps, close_caps, separators
-    local default_separator
 
     if side == 0 then
         open_caps = flexprompt.settings.tails or "flat"
@@ -626,10 +625,8 @@ local function init_segmenter(side, frame_color)
     segmenter.close_cap = close_caps[2]
 
     if segmenter.style == "lean" then
-        default_separator = "space"
-
         local available_separators = flexprompt.choices.separators
-        separators = flexprompt.settings.lean_separators or default_separator
+        separators = flexprompt.settings.lean_separators or "space"
 
         if type(separators) ~= "table" then
             separators = available_separators[separators] or separators
@@ -641,8 +638,8 @@ local function init_segmenter(side, frame_color)
         segmenter.open_cap = ""
         segmenter.close_cap = ""
     else
-        default_separator = (charset == "ascii") and "bar" or "vertical"
-
+        -- Default is chosen by "flat" caps redirecting to "bar" or "vertical"
+        -- separators.
         local available_caps = (charset == "ascii") and flexprompt.choices.ascii_caps or flexprompt.choices.caps
         local available_separators = (charset == "ascii") and flexprompt.choices.ascii_separators or flexprompt.choices.separators
         separators = flexprompt.settings.separators or flexprompt.settings.heads or "flat"
@@ -662,6 +659,9 @@ local function init_segmenter(side, frame_color)
                 local altseparators = available_separators[separators]
                 if altseparators then
                     segmenter.altseparator = altseparators[side + 1]
+                end
+                if available_caps[separators] then
+                    separators = available_caps[separators]
                 end
             end
 