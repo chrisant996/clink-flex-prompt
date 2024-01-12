9b567d3e4d62425595076e1d9a16826758c3499d
Chris Antos <sparrowhawk996@gmail.com>
2024-01-12 00:27:26 -0800

    Update release script to include bubbles.

--- make_release.cmd
+++ make_release.cmd
@@ -3,7 +3,7 @@ if "%1" == "" goto :usage
 
 set SEVENZ="c:\Program Files\7-Zip\7z.exe"
 
-%SEVENZ% a clink-flex-prompt-%1.zip flexprompt.lua flexprompt_modules.lua flexprompt_wizard.lua
+%SEVENZ% a clink-flex-prompt-%1.zip flexprompt.lua flexprompt_bubbles.lua flexprompt_modules.lua flexprompt_wizard.lua
 goto :eof
 
 :usage
